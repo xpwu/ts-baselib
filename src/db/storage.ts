5b1c55ab31e06032a29691b7ec1574465df92c7e
xpwu <diodexp@foxmail.com>
2022-07-03 03:04:35 +0800

    add memory storage

--- src/db/storage.ts
+++ src/db/storage.ts
@@ -2,34 +2,37 @@ import { ProNullable } from "ts-json";
 
 
 export interface BlStorage {
-  set<T>(key:string, value:T): Promise<void>;
-  get<T>(key:string, con:{new (...args:any[]):T}):Promise<ProNullable<T>|undefined>;
-  has(key:string):boolean
-  remove(key:string): Promise<void>;
+  set<T>(key:string, value:T):Promise<void>
+  get<T>(key:string, con:{new (...args:any[]):T}):Promise<ProNullable<T>|undefined>
+  has(key:string): Promise<boolean>
+  remove(key:string): Promise<void>
 }
 
-// export class MemoryStorage implements BlStorage{
-//   private storage = new Map<string, object>()
-//
-//   async get(key: string): Promise<object | undefined> {
-//     return this.storage.get(key);
-//   }
-//
-//   async remove(key: string): Promise<void> {
-//     this.storage.delete(key);
-//   }
-//
-//   async set(key: string, value: object): Promise<void> {
-//     this.storage.set(key, value);
-//   }
-//
-// }
+export const memoryStorage = new class ms implements BlStorage{
+  static storage = new Map<string, any>()
+
+  async get<T>(key:string, con:{new (...args:any[]):T}):Promise<ProNullable<T>|undefined> {
+    return ms.storage.get(key);
+  }
+
+  async remove(key: string): Promise<void> {
+    ms.storage.delete(key);
+  }
+
+  async set<T>(key:string, value:T): Promise<void> {
+    ms.storage.set(key, value);
+  }
+
+  async has(key:string):Promise<boolean> {
+    return ms.storage.has(key)
+  }
+}
 
 class dummyStorage implements BlStorage {
   constructor(public log: string) {
   }
 
-  has(key:string):boolean {
+  has(key:string):Promise<boolean> {
     throw Error(`must set ${this.log}, please use 'setXXXStorage'`);
   }
 
