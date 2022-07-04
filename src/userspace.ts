6b2a3c32a7b78d2a1d65adcd8115b3f05ee0b832
xpwu <diodexp@foxmail.com>
2022-07-04 22:08:18 +0800

    modify net factory

--- src/userspace.ts
+++ src/userspace.ts
@@ -1,7 +1,7 @@
 import {TableFactory} from "./db/table"
 import {DB} from "./db/db"
 import { NC } from "ts-nc"
-import {NetFactory} from "./api/net"
+import {Net} from "./api/net"
 
 
 export interface UserSpace {
@@ -9,8 +9,8 @@ export interface UserSpace {
   readonly selfDB: DB
   readonly shareDB: DB
   readonly nc: NC
-  readonly nf: NetFactory
+  readonly nf: { get(name: string): Net }
   readonly uid:string
 
-  clone(uid: string): UserSpace
+  clone(uid: string, becauseOfNet: string): UserSpace
 }
